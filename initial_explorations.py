7faa5663bb905cc398f22d558b37d17f3f82e6e1
Kayla Robinson <kaylarobinson077@gmail.com>
2019-12-28 13:10:04 -0600

    working towards labeling chapters

--- initial_explorations.py
+++ initial_explorations.py
@@ -13,13 +13,13 @@ url_war_and_peace = 'http://www.gutenberg.org/files/2600/2600-h/2600-h.htm'
 url_bible = 'http://www.gutenberg.org/files/10/10-h/10-h.htm'
 url_wizard_of_oz = 'http://www.gutenberg.org/files/55/55-h/55-h.htm'
 url_les_mis = 'http://www.gutenberg.org/files/135/135-h/135-h.htm'
+url_alice_in_wonderland = 'http://www.gutenberg.org/files/11/11-h/11-h.htm'
 
-
-def sentiment_main(url, title, chapter_breaks = False, rolling_window = False):
+def sentiment_main(url, title, window = False):
     
     tokenized_text = preprocess_text(url)
     df_sentiment = sentiment_analysis(tokenized_text)
-    ax = sentiment_plot(df_sentiment, title, chapter_breaks, rolling_window)
+    ax = sentiment_plot(df_sentiment, title, window)
     plt.show()
     
 def preprocess_text(url):
@@ -49,29 +49,43 @@ def sentiment_analysis(tokenized_text):
     df_sentiment = pd.DataFrame.from_records(results)
     return df_sentiment
 
-def sentiment_plot(df_sentiment, title, chapter_breaks, rolling_window):
-    if chapter_breaks:
-        chapter_breaks = df_sentiment[df_sentiment['sentence'].str.contains("CHAPTER")].index
-        for xc in chapter_breaks:
-            plt.axvline(x=xc, ls='-', lw=0.3, color = 'grey')
+
+
+def label_chapters(df_sentiment):
+    chapter_breaks = df_sentiment[df_sentiment['sentence'].str.contains("CHAPTER")].index
+    df_chapter_boundaries = pd.DataFrame(columns=['ch_start', 'ch_end'])
+    df_chapter_boundaries['ch_start'] = chapter_breaks
+    df_chapter_boundaries['ch_end'][0:-1] = chapter_breaks[1:]-1
+    df_chapter_boundaries['ch_end'][-1:] = df_sentiment.shape[0]
+    
+    
+
+def sentiment_plot(df_sentiment, title, window):
+    #if window == 'chapter':
+        
             
-    if rolling_window == False:
+    if window == False:
         rolling_window = int(df_sentiment.shape[0] / 20)
-    
-    df_sentiment["rolling_pos"] = df_sentiment["pos"].rolling(rolling_window).mean()
-    df_sentiment["rolling_neg"] = df_sentiment["neg"].rolling(rolling_window).mean()
-    #df_sentiment["rolling_neu"] = df_sentiment["neu"].rolling(rolling_window).mean()
-    
-    x = np.linspace(1,df_sentiment.shape[0], df_sentiment.shape[0])
-    #y = df["rolling"]
-    ax = plt.plot(x, df_sentiment["rolling_pos"], '-', color='green', lw=0.8, label = 'Positive')
-    ax = plt.plot(x, df_sentiment["rolling_neg"], '-', color='red', lw=0.8, label = 'Negative')
-    #plt.plot(x, df["rolling_neu"], '-', color='blue', lw=0.8)
-    #plt.plot(x, df["rolling_pos"]-df["rolling_neg"], '-', color='black', lw=0.8)
-    plt.xlabel('Sentence Number')
-    plt.ylabel('Fraction')
-    plt.title(title)
-    plt.legend()
+        
+    if window != 'chapter':
+        df_sentiment["rolling_pos"] = df_sentiment["pos"].rolling(rolling_window).mean()
+        df_sentiment["rolling_neg"] = df_sentiment["neg"].rolling(rolling_window).mean()
+        #df_sentiment["rolling_neu"] = df_sentiment["neu"].rolling(rolling_window).mean()
+        
+        x = np.linspace(1,df_sentiment.shape[0], df_sentiment.shape[0])
+        #y = df["rolling"]
+        ax = plt.plot(x, df_sentiment["rolling_pos"], '-', color='green', lw=0.8, label = 'Positive')
+        ax = plt.plot(x, df_sentiment["rolling_neg"], '-', color='red', lw=0.8, label = 'Negative')
+        #plt.plot(x, df["rolling_neu"], '-', color='blue', lw=0.8)
+        #plt.plot(x, df["rolling_pos"]-df["rolling_neg"], '-', color='black', lw=0.8)
+        plt.xlabel('Sentence Number')
+        plt.ylabel('Fraction')
+        plt.title(title)
+        plt.legend()
+        
+    if window == 'chapter':
+        
+        
     return ax
 
 
@@ -82,3 +96,4 @@ sentiment_main(url_war_and_peace, "War and Peace")
 sentiment_main(url_bible, "The Bible")
 sentiment_main(url_wizard_of_oz, "The Wizard of Oz")
 sentiment_main(url_les_mis, "Les Miserables")
+sentiment_main(url_alice_in_wonderland, "Alice in Wonderland")
